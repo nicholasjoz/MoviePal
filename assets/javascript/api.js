195ad8948b15f484876b51d73a8e52ff4d2ab264
discoshrimp <roydega@gmail.com>
2018-07-10 16:00:29 -0700

    added global omdb var, still wprkong on pther functipnality

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,31 +1,80 @@
-var yelp = "https://api.yelp.com/v3/businesses/search/(movietheaters)"+theaterSelect;
-var omdbData = "http://www.omdbapi.com/?t="+movieTitle+"apikey=3efbbefc";
-var omdbPoster = "http://img.omdbapi.com/?t="+moveTitle+"&apikey=3efbbefc&t=";
-var metacritic ="https://api-marcalencc-metacritic-v1.p.mashape.com/search/"+movietitle+"/movie";
-//var fandango = "http://api.fandango.com/v1/?op=theatersbypostalcodesearch&postalcode="+postCode+"&apikey=g9syenqx5p58hcztdkthzf3z&sig=9b58ee950f5f0b07283c53a74be80f6ab721f18b2377c606dcf6895937382ed8";
-var movieTitle=$("searchBox").val().trim();
+var movieTitle= "";
+
+
 $(document).ready(function(){
-	$("#searchButton").on("click", function(){
-$.ajax({
-	url: omdbData,
-	method: "GET"
-  }).then(function(omdbDataResponse) {
-	console.log(omdbDataResponse);
+	$("#searchButton").on("click", function(event){
+		movieTitle=$("#searchBox").val().trim();
+		//var yelp = "https://api.yelp.com/v3/businesses/search/(movietheaters)";
+		var omdbData = "http://www.omdbapi.com/?t="+movieTitle+"&apikey=3efbbefc";
+		var omdbPoster = "http://img.omdbapi.com/?apikey=3efbbefc&t="+movieTitle;
+		var metacritic ="https://api-marcalencc-metacritic-v1.p.mashape.com/search/" +movieTitle+ "/movie";
+		var yelp = "https://api.yelp.com/v3/businesses/search/";
 
-  });
-
-  $.ajax({
+		var omdbResponse={
+			"title":"",
+			"year":"",
+			"rated":"",
+			"released":"",
+			"runtime":"",
+			"actors":[],
+			"awards":[],
+			"boxOffice":"",
+			"country":"",
+			"director":"",
+			"genre":"",
+			"language":"",
+			"metascore":"",
+			"plot":"",
+			"poster":"",
+			"production":"",
+			"writer":"",
+			"imdbId:":"",
+			"imdbRating":0,
+			"imdbVotes":0,
+		}
+		event.preventDefault();
+		console.log("click success!");
+		console.log(movieTitle);
+		
+		/*$.ajax({
+			url: metacritic,
+			method: "GET"
+		}).then(function(metacriticResponse) {
+			console.log(metacriticResponse);
+		});*/
+		$.ajax({
+			url: omdbData,
+			method: "GET"
+			}).then(function(omdbDataResponse) {
+			console.log(omdbDataResponse);
+				omdbResponse.title=omdbDataResponse.Title
+				omdbResponse.year=omdbDataResponse.Year
+				omdbResponse.rated=omdbDataResponse.Rated
+				omdbResponse.released=omdbDataResponse.Released
+				omdbResponse.runtime=omdbDataResponse.Runtime
+				omdbResponse.actors=omdbDataResponse.Actors
+				omdbResponse.awards=omdbDataResponse.Awards
+				omdbResponse.boxOffice=omdbDataResponse.BoxOffice
+				omdbResponse.country=omdbDataResponse.Country
+				omdbResponse.director=omdbDataResponse.Director
+				omdbResponse.genre=omdbDataResponse.Genre
+				omdbResponse.language=omdbDataResponse.Language
+				omdbResponse.metascore=omdbDataResponse.Metascore
+				omdbResponse.plot=omdbDataResponse.Plot
+				omdbResponse.poster=omdbDataResponse.Poster
+				omdbResponse.production=omdbDataResponse.Production
+				omdbResponse.writer=omdbDataResponse.Writer
+				omdbResponse.imdbId=omdbDataResponse.imdbID
+				omdbResponse.imdbRating=omdbDataResponse.imdbRating
+				omdbResponse.imdbVotes=omdbDataResponse.imdbVotes
+				//return omdbResponse;
+			});
+		console.log(omdbResponse);
+		/*$.ajax({
 	url: omdbPoster,
 	method: "GET"
   }).then(function(omdbPosterResponse) {
 	console.log(omdbPosterResponse);
-  });
-
-  /*$.ajax({
-	url: fandango,
-	method: "GET"
-  }).then(function(fandangoResponse) {
-	console.log(fandangoResponse);
   });*/
 
   $.ajax({
@@ -34,5 +83,7 @@ $.ajax({
   }).then(function(yelpResponse) {
 	console.log(yelpResponse);
 	});
+	
+	//$("#posterBoy").append("<img src='"+omdbResponse.Poster+"'></img>")
 })
 })
